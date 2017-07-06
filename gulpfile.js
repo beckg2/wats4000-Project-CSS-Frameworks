f730838bdebf09796f39e567db690c995fd0e930
George <beckg2@seattleu.edu>
2017-07-05 22:26:05 -0700

    Experimenting with styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// generated on 2017-07-03 using generator-webapp 3.0.1
+// generated on 2017-07-06 using generator-webapp 3.0.1
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
